931feeb0cf1600b8e87c96712671b297a3a13c76
swholm19 <swholm19@gmail.com>
2018-08-07 16:09:42 -0400

    Removed trailing "/" from production URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://warranty-tracking-api.herokuapp.com/',
+  production: 'https://warranty-tracking-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
